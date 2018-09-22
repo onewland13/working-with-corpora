d22b92b86c8e4ec7ad11dca26904476e543cf0b7
Dylan Phelan <dtphelan1@gmail.com>
2018-09-22 09:20:58 -0400

    Finished p9

--- a2/a2-fodder.py
+++ a2/a2-fodder.py
@@ -34,15 +34,26 @@ for w in vocab_1[:600]:
             part_h = synset.part_holonyms()
             substance_h = synset.substance_holonyms()
             if (member_m or part_m or substance_m or member_m or part_m or substance_m): 
-                print('\nSynset: ', synset)
+                print('\n\nSynset: ', synset)
                 if (member_m or part_m or substance_m): 
                     print("--- meronyms")
-                    if len(member_m) > 0: print("member_m: ", synset.member_meronyms())
-                    if len(part_m) > 0: print("part_m: ", synset.part_meronyms())
-                    if len(substance_m) > 0: print("substance_m: ", synset.substance_meronyms())
+                    if len(member_m) > 0: print("member_m: A constituent member of the above Synset term: ", member_m)
+                    if len(part_m) > 0: print("part_m: Part of the above Synset term: ", part_m)
+                    if len(substance_m) > 0: print("substance_m: The substance of the above synset term: ", substance_m)
                 if (member_h or part_h or substance_h): 
                     print("--- holonymss")
-                    if len(member_h) > 0: print("member_h: ", synset.member_holonyms())
-                    if len(part_h) > 0: print("part_h: ", synset.part_holonyms())
-                    if len(substance_h) > 0: print("substance_h: ", synset.substance_holonyms())
+                    if len(member_h) > 0: print("member_h: The above Synset term is a member of: ", member_h)
+                    if len(part_h) > 0: print("part_h: The above Synset term is a part of: ", part_h)
+                    if len(substance_h) > 0: print("substance_h: The above Synset term is made of: ", substance_h) 
+
+
+for w in vocab_1[:1600]:
+    avail_synsets = wn.synsets(w)
+    if len(avail_synsets) > 0:
+        for synset in avail_synsets:
+            substance_h = synset.substance_holonyms()
+            if (substance_h): 
+                print('\n\nSynset: ', synset)
+                print("--- holonymss")
+                print("substance_h: The above Synset term is made of: ", substance_h) 
 
